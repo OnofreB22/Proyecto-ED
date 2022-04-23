46403b197fd251654d1f5c8ba841fc017549fc5a
OnofreB22 <oabenjumev@gmail.com>
2022-04-22 20:52:53 -0500

    dijkstra

--- Codigo/main.py
+++ Codigo/main.py
@@ -3,3 +3,5 @@ import networkx as nx
 
 df = pd.read_csv('calles_de_medellin_con_acoso.csv', sep = ';')
 g = nx.from_pandas_edgelist(df, source = 'origin', target = 'destination', edge_attr = 'harassmentRisk')
+
+print(nx.dijkstra_path(g,"(-75.5728593, 6.2115169)","(-75.5736973, 6.2109569)"))
