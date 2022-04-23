e07c4e161f23a721a0692ef91aafdc37ba8ff4f8
OnofreB22 <oabenjumev@gmail.com>
2022-04-22 23:11:07 -0500

    Arrelgos menores

--- Codigo/main.py
+++ Codigo/main.py
@@ -4,4 +4,5 @@ import networkx as nx
 df = pd.read_csv('calles_de_medellin_con_acoso.csv', sep = ';')
 g = nx.from_pandas_edgelist(df, source = 'origin', target = 'destination', edge_attr = 'harassmentRisk')
 
+#ruta con menos acoso usando el algoritmo Dijkstra
 print(nx.dijkstra_path(g,"(-75.5728593, 6.2115169)","(-75.5736973, 6.2109569)"))
