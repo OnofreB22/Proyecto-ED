784fa24033a39a58aa3512d3fe443e2bce7a1132
OnofreB22 <oabenjumev@gmail.com>
2022-05-16 23:18:44 -0500

    Mostrar ruta en el mapa

--- Codigo/main.py
+++ Codigo/main.py
@@ -1,8 +1,29 @@
 import pandas as pd
 import networkx as nx
+import gmplot
 
-df = pd.read_csv('calles_de_medellin_con_acoso.csv', sep = ';')
-g = nx.from_pandas_edgelist(df, source = 'origin', target = 'destination', edge_attr = 'harassmentRisk')
+df = pd.read_csv('calles_de_medellin_con_acoso.csv')
+
+#eliminar calles con riesgo mayor a r
+r = 0.9
+df = df[df['harassmentRisk'] < r]
 
 #ruta con menos acoso usando el algoritmo Dijkstra
-print(nx.dijkstra_path(g,"(-75.5728593, 6.2115169)","(-75.5736973, 6.2109569)"))
+g = nx.from_pandas_edgelist(df, source = 'origin', target = 'destination', edge_attr = 'harassmentRisk')
+ruta = nx.dijkstra_path(g,'-75.5666527, 6.2091202','-75.5715105, 6.2063061', weight = True)
+
+#listas de coordenadas
+lat = []
+lang = []
+
+for coordenadas in ruta:
+    com = coordenadas.find(',')
+    lang.append(float(coordenadas[0:com]))
+    lat.append(float(coordenadas[com+1:]))
+
+#mostrar la ruta
+gmap = gmplot.GoogleMapPlotter(lat[0],lang[0], 15)
+gmap.marker(lat[0], lang[0], color='cornflowerblue')
+gmap.marker(lat[-1], lang[-1], color='cornflowerblue')
+gmap.plot(lat, lang, 'blue', edge_width=2.5)
+gmap.draw('map.html')
