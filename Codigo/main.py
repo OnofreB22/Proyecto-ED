3e35527b8e7acbd6ae844a5bc1d5dae0097952bb
OnofreB22 <oabenjumev@gmail.com>
2022-05-17 05:59:00 -0500

    Arreglos

--- Codigo/main.py
+++ Codigo/main.py
@@ -4,13 +4,26 @@ import gmplot
 
 df = pd.read_csv('calles_de_medellin_con_acoso.csv')
 
-#eliminar calles con riesgo mayor a r
-r = 0.9
-df = df[df['harassmentRisk'] < r]
+#grafo
+g = nx.DiGraph()
+
+for index, row in df.iterrows():
+    if row['oneway'] == 'True':
+        g.add_weighted_edges_from([(row['origin'],row['destination'],row['length']), (row['destiny'],row['origin'])])
+    else:
+        g.add_weighted_edges_from([(row['origin'],row['destination'],row['length'])])
 
 #ruta con menos acoso usando el algoritmo Dijkstra
-g = nx.from_pandas_edgelist(df, source = 'origin', target = 'destination', edge_attr = 'harassmentRisk')
-ruta = nx.dijkstra_path(g,'-75.5666527, 6.2091202','-75.5715105, 6.2063061', weight = True)
+ruta = nx.dijkstra_path(g,'-75.5778046, 6.2029412','-75.6101004, 6.2312125', weight = True)
+
+#distancia mas corta o riesgo de acoso
+valor = 0
+
+for geo in range (len(ruta)-1):
+    condition = df[(df['origin'] == ruta[geo]) & (df['destination'] == ruta[geo+1])].index.values.astype(int)
+    valor = valor + df.iloc[int(condition)]['harassmentRisk']
+
+print(valor/len(ruta))
 
 #listas de coordenadas
 lat = []
@@ -25,5 +38,5 @@ for coordenadas in ruta:
 gmap = gmplot.GoogleMapPlotter(lat[0],lang[0], 15)
 gmap.marker(lat[0], lang[0], color='green')
 gmap.marker(lat[-1], lang[-1], color='red')
-gmap.plot(lat, lang, 'blue', edge_width=2.5)
+gmap.plot(lat, lang, 'blue', edge_width=4)
 gmap.draw('map.html')
